8b49e124a986d6c1a26764bb81d70ece73cefb42
Joel Concepcion <92596673+Zenbos@users.noreply.github.com>
2022-12-16 11:55:05 -0400

    Inicio de TSExpress
    Buscar colores,
    hacer un diseno,
    buscar como hacer un splash? ,
    y las imagenes y demas .

--- lib/Demo1.0/Frontend/BNavigation/Bnav4/widget/tabbar_widget.dart
+++ lib/Demo1.0/Frontend/BNavigation/Bnav4/widget/tabbar_widget.dart
@@ -13,69 +13,62 @@ class TabBarWidget extends StatelessWidget {
     Key? key,
     required this.title,
     required this.tabs,
-    required this.children, required this.icon,
+    required this.children,
+    required this.icon,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) => DefaultTabController(
-
         length: tabs.length,
-
         child: Scaffold(
           appBar: AppBar(
             title: Text(title),
-            toolbarHeight: 75,
+            toolbarHeight: 95,
             centerTitle: true,
             flexibleSpace: Container(
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Colors.black54,Colors.transparent, Colors.black54],
+                  colors: [Colors.black54, Colors.transparent, Colors.black54],
                   begin: Alignment.centerRight,
                   end: Alignment.centerLeft,
                 ),
               ),
               child: Column(
-
-                children: <Widget> [
-
+                children: <Widget>[
                   Container(
-
-                    height:  135,
+                    height: 135,
                     decoration: const BoxDecoration(
-
+                        color: Color(0xFFFF9000),
                         borderRadius: BorderRadius.only(
                           bottomLeft: Radius.circular(36),
                           bottomRight: Radius.circular(36),
-                        )
-                    ),
+                        )),
 
                     // child: MainPage(),
-
-
                   ),
                   Positioned(
                     bottom: 0,
                     left: 0,
                     right: 0,
-
                     child: Container(
                       alignment: Alignment.center,
-                      margin: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding),
                       height: 35,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(20),
                         boxShadow: [
                           BoxShadow(
-                              offset: Offset(0,10),
+                              offset: Offset(0, 10),
                               blurRadius: 50,
-                              color: kPrimaryColor2.withOpacity(0.23)
-                          ),
+                              color: kPrimaryColor2.withOpacity(0.23)),
                         ],
                       ),
                       child: Row(
-                        children: <Widget> [
+                        children: <Widget>[
                           Expanded(
                             child: TextField(
                               onChanged: (value) {},
@@ -98,15 +91,13 @@ class TabBarWidget extends StatelessWidget {
               ),
             ),
             bottom: TabBar(
-
-              padding: EdgeInsets.only(bottom: 47,top: 2),
+              padding: EdgeInsets.only(bottom: 47, top: 2),
               isScrollable: true,
               indicatorColor: Colors.redAccent,
               indicatorWeight: 5,
               indicatorSize: TabBarIndicatorSize.tab,
               tabs: tabs,
             ),
-
             elevation: 20,
             titleSpacing: 0,
           ),
---
+++ lib/TSExpress/Main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Colors.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // static final String title = 'IMSI';
+
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Turbo sim express',
+      theme: ThemeData(
+        // backgroundColor: kPrimaryColor2,
+
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: Cappbar),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+
+      // home: const MyStatefulWidget(),
+    );
+  }
+}
