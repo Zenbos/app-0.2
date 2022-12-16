61c7ff4bb8520520e3edb98b84a0316d626626d0
Joel Concepcion <92596673+Zenbos@users.noreply.github.com>
2022-12-16 11:55:05 -0400

    Inicio de TSExpress
    Buscar colores,
    hacer un diseno,
    buscar como hacer un splash? ,
    y las imagenes y demas .

---
+++ lib/TSExpress/Colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// color: Colors.black
+// Colors that we use in our app
+
+// const boton = Colors.black45;
+const bgcolor = Colors.grey;
+const Tcolor = Colors.black54;
+const Sbgcolor = Colors.white70;
+const Cappbar = Colors.red;
