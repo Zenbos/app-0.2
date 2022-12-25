d60bb77662da3d0d266e22b993e59edfaebbc604
Joel Concepcion <92596673+Zenbos@users.noreply.github.com>
2022-12-25 02:16:10 -0400

    Colors

--- lib/TSExpress/Colors.dart
+++ lib/TSExpress/Colors.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-// color: Colors.black
-// Colors that we use in our app
+// Example:
+// const kBackgroundColor = Color(0xFFFDFDFD); or
+// const bgcolor = Colors.grey;
+//Color c1 = const Color(0xFFFFFF);// fully transparent white (invisible)
+//Color c2 = const Color(0xFFFFFFFF); // fully opaque white (visible)
+
+//~~  Design mobile  ~~
+
 
 // const boton = Colors.black45;
 const bgcolor = Colors.grey;
 const Tcolor = Colors.black54;
 const Sbgcolor = Colors.white70;
 const Cappbar = Colors.red;
+const appcolor = Color(0xFF830B0B);//#6F0E0E
+
+//~~  Text design  ~~
+const colorT = Color(0xFFD32F2F);
+const colrT = Color(0xfff44336);
+//~~  Bottom  ~~
+//~~  Verificacion  ~~ (que son cuando algo sale bien o mal)
